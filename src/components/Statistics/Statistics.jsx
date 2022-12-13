dfbeab85937aafa73e9975a7dbaea700c71c83d3
Marta <maardiakiv@gmail.com>
2022-12-13 12:46:03 +0100

    transfer service function into utils dir

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,6 @@
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+import getRandomHexColor from 'services/getRandomHexColor';
 
 export const Statistics = ({ title, stats }) => {
   return (
